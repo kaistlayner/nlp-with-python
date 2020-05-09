28b217f59eb994b096a24b08e3e2e785c49dc6b4
hyojin1912 <hyojin@kaist.ac.kr>
2020-05-09 13:07:41 +0900

    타짜 대본단위 processing 완료

--- cs372.py
+++ cs372.py
@@ -51,7 +51,8 @@ def extract10():
         line = f.readline()
         if not line:
             break
-        if ':' in line:
+
+        elif ':' in line:
             character = line.split(':')[0].rstrip()
             script = line.split(':')[1].strip()
 
@@ -59,9 +60,19 @@ def extract10():
             # script = re.sub('', '', script) 이거 쓸수 있으면 이거 쓰는게 좋을듯
             while '(' in script:
                 script = script.split('(')[0] + script.replace(script.split(')')[0] + ')', '')
+            script = script.strip()
             key = character
             data[key].append(script)
-    print(data)
+
+        # 대본이 한줄 뛰고 이어지는 경우들 처리
+        elif re.match(r'[\t]', line):
+            script = line.strip()
+            while '(' in script:
+                script = script.split('(')[0] + script.replace(script.split(')')[0] + ')', '')
+            script = script.strip()
+            data[key].append(script)
+
+    print(data['고니'])
     f.close()
 
 def extract11():
@@ -81,12 +92,13 @@ def extract11():
                 script = script.split('(')[0] + script.replace(script.split(')')[0] + ')', '')
             key = character
             data[key].append(script)
+
     print(data)
     f.close()
 
 def main():
     # 영화대본모음 폴더에 들어가서 모든 txt에 대해서 txt파일명 마지막 번호 읽어와서 그에 맞는 대본 processing후 db에 삽입
-    extract9()
+    extract10()
 
 if __name__ == '__main__':
     main()
