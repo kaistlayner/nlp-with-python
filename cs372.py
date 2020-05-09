2b3d93b40bbd45c94e85faa81281dfe6bfb6b654
hyojin1912 <hyojin@kaist.ac.kr>
2020-05-09 12:26:31 +0900

    각 영화 processing 하는 틀로 변형

--- cs372.py
+++ cs372.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 import re
 
-def main():
-    f = open("./타짜.txt", 'rt', encoding='UTF8')
+def extract2():
+    f = open("./영화대본모음/끝까지간다.txt", 'rt', encoding='UTF8')
     data = defaultdict(list)
     while True:
         line = f.readline()
@@ -22,8 +22,71 @@ def main():
             key = character
             data[key].append(script)
     print(data)
-
     f.close()
+
+def extract9():
+    f = open("./영화대본모음/악마를보았다.txt", 'rt', encoding='UTF8')
+    data = defaultdict(list)
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        if ':' in line:
+            character = line.split(':')[0].rstrip()
+            script = line.split(':')[1].strip()
+
+            # (혼잣말하듯) 이러한 행동지시 ()부분을 모두 대본에서 지운다
+            # script = re.sub('', '', script) 이거 쓸수 있으면 이거 쓰는게 좋을듯
+            while '(' in script:
+                script = script.split('(')[0] + script.replace(script.split(')')[0] + ')', '')
+            key = character
+            data[key].append(script)
+    print(data)
+    f.close()
+
+def extract10():
+    f = open("./영화대본모음/타짜.txt", 'rt', encoding='UTF8')
+    data = defaultdict(list)
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        if ':' in line:
+            character = line.split(':')[0].rstrip()
+            script = line.split(':')[1].strip()
+
+            # (혼잣말하듯) 이러한 행동지시 ()부분을 모두 대본에서 지운다
+            # script = re.sub('', '', script) 이거 쓸수 있으면 이거 쓰는게 좋을듯
+            while '(' in script:
+                script = script.split('(')[0] + script.replace(script.split(')')[0] + ')', '')
+            key = character
+            data[key].append(script)
+    print(data)
+    f.close()
+
+def extract11():
+    f = open("./영화대본모음/파수꾼.txt", 'rt', encoding='UTF8')
+    data = defaultdict(list)
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        if ':' in line:
+            character = line.split(':')[0].rstrip()
+            script = line.split(':')[1].strip()
+
+            # (혼잣말하듯) 이러한 행동지시 ()부분을 모두 대본에서 지운다
+            # script = re.sub('', '', script) 이거 쓸수 있으면 이거 쓰는게 좋을듯
+            while '(' in script:
+                script = script.split('(')[0] + script.replace(script.split(')')[0] + ')', '')
+            key = character
+            data[key].append(script)
+    print(data)
+    f.close()
+
+def main():
+    # 영화대본모음 폴더에 들어가서 모든 txt에 대해서 txt파일명 마지막 번호 읽어와서 그에 맞는 대본 processing후 db에 삽입
+    extract9()
 
 if __name__ == '__main__':
     main()
